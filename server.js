85b810f8124bbc94d94b9bde0d61317f13b6cf7b
Muneeb Mustafa <muneeb.mustafa080@gmail.com>
2024-11-23 17:29:57 +0500

    File change4s

--- server.js
+++ server.js
@@ -12,11 +12,16 @@ DbConnect()
 app.use(express.json())
 app.use(cors({
   origin: ['https://todo-frontend-muneeb-mustafas-projects.vercel.app'],
+  'http://localhost:3000',
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   credentials: true,
 }))
 
 app.use('/api', router)
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Todo App API!');
+});
 
 // Correctly reference process.env.PORT
 app.listen(process.env.PORT, () => {
